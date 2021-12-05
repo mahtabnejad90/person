ec94f234a8b701f462676717bf5931527d281bbb
Mahtab Nejad <mahtab@bahmannejad.com>
2021-12-05 23:12:28 +0000

    spring boot learning initial commit

---
+++ src/main/java/com/example/person/service/PersonService.java
@@ -0,0 +1,44 @@
+package com.example.person.service;
+
+import com.example.person.dao.PersonDao;
+import com.example.person.model.Person;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+
+@Service
+public class PersonService {
+
+    private final PersonDao personDao;
+
+    @Autowired //autowiring into DAO interface
+    public PersonService (@Qualifier("postgres") PersonDao personDao){
+        this.personDao = personDao;
+    }
+    public int addPerson(Person person){
+        return personDao.insertPerson(person);
+    }
+
+    public List<Person> getAllPeople(){
+        return personDao.selectAllPeople();
+    }
+
+        public Optional<Person> getPersonById(UUID id){
+        return personDao.selectPersonById(id);
+        }
+
+        public int deletePerson(UUID id){
+        return personDao.deletePersonById(id);
+        }
+
+        public int updatePerson(UUID id, Person newPerson){
+        return personDao.updatePersonById(id, newPerson);
+        }
+
+}
+
